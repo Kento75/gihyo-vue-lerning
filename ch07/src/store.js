f4e735f3ed4642aa7321c6b0cc4729fa5edf513b
Kento75 <kento2github@gmail.com>
2018-10-24 22:12:58 +0900

    Add filter and input form

--- ch07/src/store.js
+++ ch07/src/store.js
@@ -21,7 +21,88 @@ const store = new Vuex.Store({
         done: true
       }
     ],
-  }
+
+    // ラベルの初期ステート
+    labels: [
+      {
+        id: 1,
+        text: '買い物'
+      },
+      {
+        id: 2,
+        text: '食料'
+      },
+      {
+        id: 3,
+        text: '本'
+      }
+    ],
+
+    // 次に追加するタスク、ラベルのID
+    // 実際のアプリではサーバーで生成したり、UUIDを使ったりするがここでは決め打ち
+    nextTaskId: 3,
+    nextLabelId: 4,
+
+    // フィルタするラベルのID
+    filter: null
+  },
+
+  getters: {
+    // フィルタ後のタスクを返す
+    filteredTasks(state) {
+      // ラベルが選択されていなければそのままの一覧を返す
+      if(!state.filter) {
+        return state.tasks
+      }
+
+      // 選択されているラベルでフィルタリングする
+      return state.tasks.filter(task => {
+        return task.labelIds.indexOf(state.filter) >= 0
+      })
+    },
+  },
+
+  mutations: {
+    // タスクを追加する
+    addTask(state, {name, labelIds}) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        labelIds,
+        done: false
+      })
+
+      // 次に追加されるタスクに付与するIDを更新する
+      state.nextTaskId++
+    },
+    
+    // タスクの完了状態を変更する
+    toggleTaskStatus(state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+
+      filtered.forEach(task => {
+        task.done = !task.done
+      })
+    },
+
+    // ラベルを追加する
+    addLabel(state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      })
+
+      // 次に追加されるラベルに付与するIDを更新する
+      state.nextLabelId++
+    },
+
+    // フィルタリング対象のラベルを変更する
+    changeFilter(state, { filter }) {
+      state.filter = filter
+    },
+  },
 })
 
 // ストアをエクスポート
